b27133df1ff06e7e0d6a7f682aabfa4a049ae8db
Jmarti101 <131412868+Jmarti101@users.noreply.github.com>
2023-05-14 20:41:24 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const index = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const dbService = require('./dbService');
+
+
+index.use(cors());
+index.use(express.json());
+index.use(express.urlencoded({extended: false}));
+
+
+//create
+index.post('/insert', (request, response) => {
+    const { name } = request.body;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.insertNewName(name);
+
+    result
+    .then(data => response.json({ data: data}))
+    .catch(err => console.log(err));
+});
+
+
+//read
+index.get('/getAll' ,(request, response) => {
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.getAllData();
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+    
+})
+//update
+index.patch('/update' , (request, response) => {
+    //console.log(request.body);
+    const {id , name } = request.body;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.updateNameById(id, name);
+
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+
+});
+
+//delete
+index.delete('/delete/:id', (request, response) => {
+    const { id } = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.deleteRowById(id);
+
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+});
+
+index.get('/search/:name', (request,response) => {
+    const { name } = request.params;
+    const db =dbService.getDbServiceInstance();
+})
+
+index.listen(process.env.PORT, () => console.log('app is running'));
